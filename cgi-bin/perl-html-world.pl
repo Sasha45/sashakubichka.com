9306102a365a3457b33fd922588ede4505ff32e8
Sasha <okubichk@ucsd.edu>
2022-04-27 22:02:15 -0700

    one php

--- cgi-bin/perl-html-world.pl
+++ cgi-bin/perl-html-world.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-print "Cache-Control: no-cache";
-print "Content-type: text/html\n";
+print "Cache-Control: no-cache\n";
+print "Content-type: text/html\n\n";
 print "<html>";
 print "<head>";
 print "<title>Hello, Perl!</title>";
---
+++ cgi-bin/php-html-world.php
@@ -0,0 +1,24 @@
+<?php
+
+print "Cache-Control: no-cache\n";
+print "Content-type: text/html\n\n";
+print "<html>";
+print "<head>";
+print "<title>Hello, Perl!</title>";
+print "</head>";
+print "<body>";
+
+print "<h1>Sasha was here - Hello, PHP!</h1>";
+print "<p>This page was generated with the PHP programming langauge</p>";
+
+$date = date("Y-m-d h:i:sa");
+echo "<p>Current Time:" . $date . "</p>";
+
+# IP Address is an environment variable when using CGI
+$address = $_SERVER['REMOTE_ADDR'];
+echo "<p>Your IP Address:" . $address . "</p>";
+
+print "</body>";
+print "</html>";
+
+?>
